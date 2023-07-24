87e107010d7e96074ec3be0adcb7d6a12997b7bd
chromesight <tljoshh+blue@gmail.com>
2023-07-23 23:39:06 -0400

    Ensure highlighter and ignorator target search page

--- src/contents/threadList.ts
+++ src/contents/threadList.ts
@@ -4,7 +4,7 @@ import threadListFeatures from '~lib/features/threadList';
 
 // Content script configuration
 export const config: PlasmoCSConfig = {
-	matches: ['*://*.websight.blue/', '*://*.websight.blue/threads/*'],
+	matches: ['*://*.websight.blue/', '*://*.websight.blue/threads/*', '*://*.websight.blue/search/*'],
 };
 
 injectFeatures(threadListFeatures);
