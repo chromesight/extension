c15b0f597983ecb1fba6a771035afea8e11a0595
chromesight <tljoshh+blue@gmail.com>
2023-07-28 15:43:20 -0400

    Fix grammatical errors

--- src/components/options/about/index.tsx
+++ src/components/options/about/index.tsx
@@ -5,8 +5,8 @@ export default function AboutOptions() {
 		<div className={style.group} id="about">
 			<h3 className={style.heading}>About</h3>
 			<p className={style.label}>ChromeSight by Joshh.</p>
-			<p className={style.label}>Looking for a Firefox alternative to this extension? Consider using <a href="https://addons.mozilla.org/en-US/firefox/addon/foxyeti/" target="_blank">FoxYeti by Cosban</a>.</p>
-			<p className={style.description}>This extension is inspired by ChromeLL. Thanks to Pokemans, Sonicmax, Seiru, Cosban, plus all other maintainers & contributers of the years. If you contributed to ChromeLL and I missed your name, please let me know.</p>
+			<p className={style.label}>Looking for a Firefox alternative to this extension? Consider using <a href="https://addons.mozilla.org/en-US/firefox/addon/foxyeti/" target="_blank">FoxYeti by cosban</a>.</p>
+			<p className={style.description}>This extension is inspired by ChromeLL. Thanks to Pokemans, Sonicmax, Seiru, cosban, plus all other maintainers & contributers over the years. If you contributed to ChromeLL and I missed your name, please let me know.</p>
 			<p className={style.description}>Thanks to LlamaGuy :) &amp; Tiko for obvious reasons.</p>
 		</div>
 	);
