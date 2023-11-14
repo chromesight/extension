b50c6a2d0f4fd1af34045ae5845b26e509e03884
chromesight <tljoshh+blue@gmail.com>
2023-11-14 01:50:56 -0500

    Add markdown button to create a topic page

---
+++ src/lib/features/newTopic/index.ts
@@ -0,0 +1,8 @@
+import type { Feature } from '../feature';
+import markdownButtons from '../postList/markdownButtons';
+
+const features: Array<Feature> = [
+	markdownButtons,
+];
+
+export default features;
