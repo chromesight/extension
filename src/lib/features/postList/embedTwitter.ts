905a85db606cca95b19f71957dc21657babf07eb
chromesight <tljoshh+blue@gmail.com>
2023-11-14 13:32:59 -0500

    Address race condition

--- src/lib/features/postList/embedTwitter.ts
+++ src/lib/features/postList/embedTwitter.ts
@@ -26,6 +26,10 @@ async function insertEmbeds(links: NodeListOf<HTMLAnchorElement>) {
 			const embed = await fetchEmbed(link.href.replace('x.com', 'twitter.com'));
 			if (embed) {
 				link.insertAdjacentHTML('afterend', embed.html);
+				requestAnimationFrame(() => {
+					// Wait for the markup to be inserted and rendered to the DOM. We do this to ensure the Twitter Widget script in the MAIN world won't attempt to load the tweet after the markup is inserted but before it's rendered. Sending a postMessage after inserting markup causes the event to fire before the markup has been rendered, so we wait for the animation frame.
+					window.postMessage({ type: 'load_twitter_widgets', id: link.closest('.message').id });
+				});
 			}
 		});
 }
@@ -49,20 +53,20 @@ export default createFeature(
 		eventListener.src = newWidget;
 		document.head.appendChild(eventListener);
 
-		const links: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('.message-contents p a[href*="twitter.com"], .message-contents p a[href*="x.com"]');
-		insertEmbeds(links);
+		// Hacky way of waiting for oembed markup to be added before firing message to main world to load twitter widget from markup.
+		// Source: https://macarthur.me/posts/when-dom-updates-appear-to-be-asynchronous#option-2-fire-after-next-repaint
+		// Execution of requestAnimationFrame. Occurs before next repaint.  Nesting another requestAnimationFrame will fire it's callback  after next repaint (because it waits for the animation frame after the next one to paint)
+		requestAnimationFrame(() => {
+			const links: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('.message-contents p a[href*="twitter.com"], .message-contents p a[href*="x.com"]');
+			// Nesting requestAnimationFrame call inside insertEmbeds means it fires _after_ the next repaint
+			insertEmbeds(links);
+		});
 	},
 	async (addedNode) => {
-		// Hacky way of waiting for oembed markup to be added.
-		// Source: https://macarthur.me/posts/when-dom-updates-appear-to-be-asynchronous#option-2-fire-after-next-repaint
 		requestAnimationFrame(() => {
 			// Fires _before_ next repaint
 			const links: NodeListOf<HTMLAnchorElement> = addedNode.querySelectorAll('.message-contents p a[href*="twitter.com"], .message-contents p a[href*="x.com"]');
 			insertEmbeds(links);
-			requestAnimationFrame(() => {
-				// Fires after the next repaint
-				window.postMessage({ type: 'load_twitter_widgets', addedNodeId: addedNode.id });
-			});
 		});
 	},
 );
