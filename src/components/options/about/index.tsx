7b5cad0dc846eccebcd609bb0007d2713ce95a72
tljoshh <tljoshh+blue@gmail.com>
2023-11-17 00:28:25 -0500

    Fix stylization of foxyeti

--- src/components/options/about/index.tsx
+++ src/components/options/about/index.tsx
@@ -5,9 +5,9 @@ export default function AboutOptions() {
 		<div className={style.group} id="about">
 			<h3 className={style.heading}>About</h3>
 			<p className={style.label}>ChromeSight by Joshh.</p>
-			<p className={style.label}>Looking for a Firefox alternative to this extension? Consider using <a href="https://addons.mozilla.org/en-US/firefox/addon/foxyeti/" target="_blank">FoxYeti by cosban</a>.</p>
-			<p className={style.description}>This extension is inspired by ChromeLL. Thanks to Pokemans, Sonicmax, Seiru, cosban, plus all other maintainers & contributers over the years. If you contributed to ChromeLL and I missed your name, please let me know.</p>
-			<p className={style.description}>Thanks to LlamaGuy :) &amp; Tiko for obvious reasons.</p>
+			<p className={style.label}>Looking for a Firefox alternative to this extension? Consider using <a href="https://addons.mozilla.org/en-US/firefox/addon/foxyeti/" target="_blank">foxyeti by cosban</a>.</p>
+			<p className={style.description}>This extension is inspired by ChromeLL. Thanks to Pokemans, Sonicmax, Seiru, cosban, plus all other contributors.</p>
+			<p className={style.description}>Thanks to LlamaGuy &amp; Tiko.</p>
 		</div>
 	);
 }
